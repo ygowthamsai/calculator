8e71079a453ec20a60fecb93aba32ee5426c7ab4
ygowthamsai <yakkatigowtham@gmail.com>
2023-05-06 11:25:40 +0530

    add substraction, multiplication, division and add styles.css

--- javascript.js
+++ javascript.js
@@ -1,9 +1,34 @@
-function calculate() {
+function calculate(operation) {
   var ip1 = parseFloat(document.getElementById("input1").value);
   console.log("input1 value", ip1);
   var ip2 = parseFloat(document.getElementById("input2").value);
   console.log("addition", ip1 + ip2);
   // var result = document.createElement()
   var result = document.getElementById("result");
-  result.innerText = ip1 + ip2;
+
+  //   if (operation === "add") {
+  //     result.innerText = ip1 + ip2;
+  //   } else if (operation === "sub") {
+  //     result.innerText = ip1 - ip2;
+  //   } else if (operation === "mul") {
+  //     result.innerText = ip1 * ip2;
+  //   } else result.innerText = ip1 / ip2;
+
+  switch (operation) {
+    case "add": {
+      result.innerText = ip1 + ip2;
+      break;
+    }
+    case "sub": {
+      result.innerText = ip1 - ip2;
+      break;
+    }
+    case "mul": {
+      result.innerText = ip1 * ip2;
+      break;
+    }
+    case "div": {
+      result.innerText = ip1 / ip2;
+    }
+  }
 }
